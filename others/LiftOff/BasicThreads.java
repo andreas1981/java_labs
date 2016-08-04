ee0cb75aa2cbd53f172bf8c73501f338da3acd74
andrew.kuzmenko <andrey.kuzmenko1981@gmail.com>
2016-08-04 14:18:43 +0300

    first add

---
+++ others/LiftOff/BasicThreads.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Andrew_Elena on 13.07.2016.
+ */
+public class BasicThreads {
+    public static void main(String[] args) {
+        Thread t = new Thread(new LiftOff());
+        t.start();
+        System.out.println("Waiting for LiftOff");
+    }
+}
---
+++ others/LiftOff/MainThread.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrew_Elena on 13.07.2016.
+ */
+public class MainThread {
+    public static void main(String[] args) {
+        LiftOff launch = new LiftOff();
+        launch.run();
+    }
+}
